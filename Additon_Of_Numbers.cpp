4b2f365ba9fa829059cf160262700d933f030a92
Benineza ketsia <kbenineza@gmail.com>
2024-08-02 11:21:25 +0200

    C++_CAT

---
+++ Additon_Of_Numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int number1;
+	int number2;
+	cout<<"Enter first number:";
+	cin>>number1;
+	cout<<"Enter second number:";
+	cin>>number2;
+	int result=number1+number2;
+	cout<<result;
+}
+
---
+++ Factorial.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int factorial;
+	int number;
+	cout<<"Enter a number:";
+	cin>>number;
+	for(int i=1; i<number; i--)
+	int factorial=i*number;
+	{
+		cout<<factorial<<endl;
+	}
+
+}
